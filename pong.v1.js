b51a5b3eddd5c23037684d720cb6dbc16fa06ccb
Neur0plasticity <41029495+Neur0plasticity@users.noreply.github.com>
2021-10-01 15:08:00 -0700

    Update pong.v1.js

--- pong.v1.js
+++ pong.v1.js
@@ -1,31 +1,32 @@
 
 function PONG(){
-  window.onkeydown = function paddlePosition(evt) {
-    lUP:  evt.keyCode === 87 && paddleL.y += 1;  // currently affected by framerates
-    lDOWN:evt.keyCode === 83 && paddleL.y += -1; // currently affected by framerates
-    rUP:  evt.keyCode === 38 && paddleR.y += 1;  // currently affected by framerates
-    rDOWN:evt.keyCode === 40 && paddleR.y += -1; // currently affected by framerates
-  }
   const draw = { fps: 200; seq: `arena net score paddle ball` };
+  window.onkeydown = function paddlePosition(evt) {
+    lUP:  evt.keyCode === 87 && mem.paddleL.y += 1;  // currently affected by framerates
+    lDOWN:evt.keyCode === 83 && mem.paddleL.y += -1; // currently affected by framerates
+    rUP:  evt.keyCode === 38 && mem.paddleR.y += 1;  // currently affected by framerates
+    rDOWN:evt.keyCode === 40 && mem.paddleR.y += -1; // currently affected by framerates
+  }
   let mem = {
-    arena  : { x:  0, y:0 , left: 200, right: 200, top: 100, bottom: 100, color: "black" },
-    net    : { x:  0, y:0 , left:   5, right:   5, top: 100, bottom: 100, color: "yellow"},
-    scoreL : { x: 20, y:80, left:  10, right:  10, top:  10, bottom:  10, color: "pink", cnt:0 },
-    scoreR : { x: 20, y:80, left:  10, right:  10, top:  10, bottom:  10, color: "pink", cnt:0 },
-    paddleL: { x:-180,y: 0, left:   7, right:   7, top:  23, bottom:  23, color: "white" },
-    paddleR: { x: 180,y: 0, left:   7, right:   7, top:  23, bottom:  23, color: "white" },
-    ball   : { x:   0,y: 0, radius: 5,                       bottom:  23, color: "blue"  }
+    arena  : { x:  0, y:0 , width: 400, height: 200, color: "black",   border: "3px blue solid"},
+    net    : { x:  0, y:0 , width:  10, height: 200, color: "yellow",  border: "3px red dashed"},
+    scoreL : { x: 20, y:80, width:  20, height:  20, color: "pink",    border: "1px grey none", cnt:0 },
+    scoreR : { x: 20, y:80, width:  20, height:  20, color: "pink",    border: "1px grey none", cnt:0 },
+    paddleL: { x:-180,y: 0, width:  14, height:  46, color: "white",   border: "1px green solid"},
+    paddleR: { x: 180,y: 0, width:  14, height:  46, color: "white",   border: "1px yellow solid"},
+    ball   : { x:   0,y: 0, radius: 5,               color: "blue",    border: "1px pink solid"}
   };
   const play = (sel) => {
       setInterval(()=>{        
         let str = "";
-        mem.forEach((o, idx)=>{ defaults(o, idx); uis[sel](str,o,idx); })
+        Object.keys(mem).forEach((key, idx)=>{ let o = mem[key]; defaults(o, idx); uis[sel](str,o,idx); })
         document.body.innerHTML = html;
       },1000 / draw.fps);
   }
   
   const defaults = (o)=>{
-      defaults for mem
+//       defaults for mem
+      
   }
   
   const uis = {
